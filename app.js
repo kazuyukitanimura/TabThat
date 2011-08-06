e54c3a4e2c4ee77ba282b6a598a446e28930c435
kazuyukitanimura <kazuyukitanimura@gmail.com>
2011-08-06 14:56:02 -0700

    testinputs

--- app.js
+++ app.js
@@ -4,6 +4,8 @@
  */
 
 var express = require('express');
+var MemoryStore = express.session.MemoryStore;
+var sessionStore = new MemoryStore({ reapInterval: 60000 * 10 });
 
 var app = module.exports = express.createServer();
 
@@ -12,10 +14,14 @@ var app = module.exports = express.createServer();
 app.configure(function(){
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
+  app.use(express.cookieParser());
+  app.use(express.session({secret: 'himitsu!', fingerprint: function(req){return req.socket.remoteAddress;}, store: sessionStore, key: 'express.sid'}));
   app.use(express.bodyParser());
   app.use(express.methodOverride());
+  app.use(express.compiler({ src: __dirname + '/public', enable: ['less'] }));
   app.use(app.router);
   app.use(express.static(__dirname + '/public'));
+  app.use(express.logger({ format: ':method :url' }));
 });
 
 app.configure('development', function(){
@@ -29,8 +35,23 @@ app.configure('production', function(){
 // Routes
 
 app.get('/', function(req, res){
+  var numPpl = 4
+  var expenses = []; // index == userID
+  for(var i=numPpl; i--;){
+    expenses.push(Math.ceil(Math.random()*100)); // between 0 and 100
+  }
+  var oweTable = expenses.map(function(x){
+    return expenses.map(function(y){
+      return x-y;
+    });
+  }); // 2D Array
   res.render('index', {
-    title: 'Express'
+    title: 'TabThat',
+    expense0 : expenses[0],
+    expense1 : expenses[1],
+    expense2 : expenses[2],
+    expense3 : expenses[3],
+    oweTable : oweTable
   });
 });
 
