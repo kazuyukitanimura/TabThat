4a257ee96371ba0cf2518ce0cb2713b1e919ebce
kazuyukitanimura <kazuyukitanimura@gmail.com>
2011-08-15 23:11:21 -0700

    not working

--- app.js
+++ app.js
@@ -37,168 +37,154 @@ app.configure('production', function(){
 
 app.get('/', function(req, res){
   var numPpl = 4;
-  var total = 0;
   var expenses = []; // index == userID
   for(var i=numPpl; i--;){
     expenses.push(Math.ceil(Math.random()*100)); // between 0 and 100
   }
-  for(var i=numPpl; i--;){
-    total += expenses[i];
-  }
-  //var expenses = [51,19,16,57]; // index == userID
-  //var expenses = [70,19,57,76]; // index == userID
-  //var expenses = [27,24,51,87]; // index == userID
-  //var expenses = [98,75,77,17,15];
-
-  // NO Optimization
-  var naiveTable = expenses.map(function(x, i){
-    return expenses.map(function(y, j){
-      if (i===j){
-        return 0;
-      }
-      else{
-        return y; // divide by numPpl later
-      }
-    });
-  }); // 2D Array
-  // sanity check
-  print2DArray(naiveTable);
-
-  // 1st Optimization: eliminate the bidirectional $ flow O(n**2)
-  // 1st optimization guarantees that the graph is DAG and has no bidirectional edges
-  var oweTable = expenses.map(function(x){
-    return expenses.map(function(y){
-      return y-x; // divide by numPpl later
-    });
-  }); // 2D Array
-  // sanity check
-  print2DArray(oweTable);
-
-  //var oweTable = [[  0,  0,  0,  5,  5,  0],
-  //                [  0,  0,  0,  0,  5,  5],
-  //                [  0,  0,  0, 15,  0,  0],
-  //                [ -5,  0,-15,  0,  0,  0],
-  //                [ -5, -5,  0,  0,  0,  0],
-  //                [  0, -5,  0,  0,  0,  0]];
-  //var oweTable = [[  0,  0,  0,  5,  5],
-  //                [  0,  0,  0, 10, 10],
-  //                [  0,  0,  0,  5, 25],
-  //                [ -5,-10, -5,  0,  0],
-  //                [ -5,-10,-25,  0,  0]];
-  //var numPpl = oweTable.length;
-
-  // 2nd Optimization: reduce the # of edges O(N**2 *logN)
-  // 2nd optimization guarantees that the graph is a bipartite graph, i.e. the graph diameter is 1
-  var oweTableNew = [];
-  var subTotals = [];
-  var idxArray = [];
-  for(var i=0; i<numPpl; i++){
-    var oweArray = [];
-    var subtotal = 0;
-    for(var j=0; j<numPpl; j++){
-      oweArray[j] = 0;
-      subtotal += oweTable[i][j];
-    }
-    oweTableNew[i] = oweArray;
-    subTotals[i] = subtotal;
-    idxArray[i] = i;
-  }
-  idxArray.sort(function(i, j){
-    return subTotals[i] - subTotals[j];
-  });
-  while(subTotals[idxArray[0]]!==0){
-    var owner = idxArray[numPpl-1]; // pay
-    var ownee = idxArray[0]; // receive
-    var minOwe = Math.min(subTotals[owner], -subTotals[ownee]);
-    oweTableNew[owner][ownee] =  minOwe;
-    oweTableNew[ownee][owner] = -minOwe;
-    subTotals[owner] -= minOwe;
-    subTotals[ownee] += minOwe;
-    idxArray.sort(function(i, j){
-      return subTotals[i] - subTotals[j];
-    });
-  }
-  // sanity check
-  print2DArray(oweTableNew);
-  for(var i=subTotals; i--;){
-    assert.ok(subTotals[i]===0);
-  }
-  //oweTable = transpose(oweTableNew);
-
-  var nt  = divide2DArrayByX(naiveTable,  numPpl);
-  var ot  = divide2DArrayByX(oweTableNew, numPpl);
-  var ot2 = negativeToZero(ot);
   res.render('index', {
     title: 'TabThat',
-    expenses : expenses,
-    total : total,
-    naive : nt,
-    opt : ot2
+    expenses : expenses
   });
-
-  function print2DArray(t){
-    assert.ok(t.length===t[0].length);
-    console.dir(t);
-    for(var i=0; i<t.length; i++){
-      var subtotal = 0;
-      for(var j=0; j<t[i].length; j++){
-        subtotal += t[i][j];
-      }
-      console.log(i+": "+subtotal);
-    }
-    console.log("");
-  }
-
-  function transpose(baseArray){
-    assert.ok(baseArray.length===baseArray[0].length);
-    var newArray = [];
-    for(var i=0; i<baseArray.length; i++){
-      newArray.push([]);
-      for(var j=0; j<baseArray.length; j++){
-        newArray[i][j] = baseArray[j][i];
-      }
-    }
-    return newArray;
-  }
-
-  function copy2DArray(baseArray){
-    var newArray = [];
-    for(var i=0; i<baseArray.length; i++){
-      newArray.push([]);
-      for(var j=0; j<baseArray[0].length; j++){
-        newArray[i][j] = baseArray[i][j];
-      }
-    }
-    return newArray;
-  }
-  
-  function divide2DArrayByX(baseArray, x){
-    var newArray = [];
-    for(var i=0; i<baseArray.length; i++){
-      newArray.push([]);
-      for(var j=0; j<baseArray[0].length; j++){
-        newArray[i][j] = baseArray[i][j] / x;
-      }
-    }
-    return newArray;
-  }
-
-  function negativeToZero(baseArray){
-    var newArray = [];
-    for(var i=0; i<baseArray.length; i++){
-      newArray.push([]);
-      for(var j=0; j<baseArray[0].length; j++){
-        if(baseArray[i][j]<0){
-          newArray[i][j] = 0;
-        }
-        else{
-          newArray[i][j] = baseArray[i][j];
-        }
-      }
-    }
-    return newArray;
-  }
 });
 
 app.listen(3000);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
+
+var dnode = require('dnode');
+var server = dnode({
+      optimize : function(expenses, cb){
+        var numPpl = expenses.length;
+
+        // NO Optimization
+        var naiveTable = expenses.map(function(x, i){
+          return expenses.map(function(y, j){
+            if (i===j){
+              return 0;
+            }
+            else{
+              return y; // divide by numPpl later
+            }
+          });
+        }); // 2D Array
+        // sanity check
+        print2DArray(naiveTable);
+      
+        // 1st Optimization: eliminate the bidirectional $ flow O(n**2)
+        // 1st optimization guarantees that the graph is DAG and has no bidirectional edges
+        var oweTable = expenses.map(function(x){
+          return expenses.map(function(y){
+            return y-x; // divide by numPpl later
+          });
+        }); // 2D Array
+        // sanity check
+        print2DArray(oweTable);
+      
+        // 2nd Optimization: reduce the # of edges O(N**2 *logN)
+        // 2nd optimization guarantees that the graph is a bipartite graph, i.e. the graph diameter is 1
+        var oweTableNew = [];
+        var subTotals = [];
+        var idxArray = [];
+        for(var i=0; i<numPpl; i++){
+          var oweArray = [];
+          var subtotal = 0;
+          for(var j=0; j<numPpl; j++){
+            oweArray[j] = 0;
+            subtotal += oweTable[i][j];
+          }
+          oweTableNew[i] = oweArray;
+          subTotals[i] = subtotal;
+          idxArray[i] = i;
+        }
+        idxArray.sort(function(i, j){
+          return subTotals[i] - subTotals[j];
+        });
+        while(subTotals[idxArray[0]]!==0){
+          var owner = idxArray[numPpl-1]; // pay
+          var ownee = idxArray[0]; // receive
+          var minOwe = Math.min(subTotals[owner], -subTotals[ownee]);
+          oweTableNew[owner][ownee] =  minOwe;
+          oweTableNew[ownee][owner] = -minOwe;
+          subTotals[owner] -= minOwe;
+          subTotals[ownee] += minOwe;
+          idxArray.sort(function(i, j){
+            return subTotals[i] - subTotals[j];
+          });
+        }
+        // sanity check
+        print2DArray(oweTableNew);
+        for(var i=subTotals; i--;){
+          assert.ok(subTotals[i]===0);
+        }
+        //oweTable = transpose(oweTableNew);
+      
+        var nt  = divide2DArrayByX(naiveTable,  numPpl);
+        var ot  = divide2DArrayByX(oweTableNew, numPpl);
+        var ot2 = negativeToZero(ot);
+
+        cb(nt, ot2);
+      }
+    });
+server.listen(app);
+
+function print2DArray(t){
+  assert.ok(t.length===t[0].length);
+  console.dir(t);
+  for(var i=0; i<t.length; i++){
+    var subtotal = 0;
+    for(var j=0; j<t[i].length; j++){
+      subtotal += t[i][j];
+    }
+    console.log(i+": "+subtotal);
+  }
+  console.log("");
+}
+
+function transpose(baseArray){
+  assert.ok(baseArray.length===baseArray[0].length);
+  var newArray = [];
+  for(var i=0; i<baseArray.length; i++){
+    newArray.push([]);
+    for(var j=0; j<baseArray.length; j++){
+      newArray[i][j] = baseArray[j][i];
+    }
+  }
+  return newArray;
+}
+
+function copy2DArray(baseArray){
+  var newArray = [];
+  for(var i=0; i<baseArray.length; i++){
+    newArray.push([]);
+    for(var j=0; j<baseArray[0].length; j++){
+      newArray[i][j] = baseArray[i][j];
+    }
+  }
+  return newArray;
+}
+
+function divide2DArrayByX(baseArray, x){
+  var newArray = [];
+  for(var i=0; i<baseArray.length; i++){
+    newArray.push([]);
+    for(var j=0; j<baseArray[0].length; j++){
+      newArray[i][j] = baseArray[i][j] / x;
+    }
+  }
+  return newArray;
+}
+
+function negativeToZero(baseArray){
+  var newArray = [];
+  for(var i=0; i<baseArray.length; i++){
+    newArray.push([]);
+    for(var j=0; j<baseArray[0].length; j++){
+      if(baseArray[i][j]<0){
+        newArray[i][j] = 0;
+      }
+      else{
+        newArray[i][j] = baseArray[i][j];
+      }
+    }
+  }
+  return newArray;
+}
--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -1,1 +1,48 @@
-$(function(){});
+$(function(){
+  DNode.connect(function (remote) {
+    $('input').live('change keyup', function(){
+      var expenses = $('input').map(function(){
+            return $(this).val();
+          });
+      var total = 0;
+      for(var i=expenses.length; i--;){
+        total = expenses[i];
+      }
+      $('#total').text(total);
+      remote.optimize(expenses, function(naive, opt){
+        $('#naive00').text(naive[0][0]);
+        $('#naive01').text(naive[0][1]);
+        $('#naive02').text(naive[0][2]);
+        $('#naive03').text(naive[0][3]);
+        $('#naive10').text(naive[1][0]);
+        $('#naive11').text(naive[1][1]);
+        $('#naive12').text(naive[1][2]);
+        $('#naive13').text(naive[1][3]);
+        $('#naive20').text(naive[2][0]);
+        $('#naive21').text(naive[2][1]);
+        $('#naive22').text(naive[2][2]);
+        $('#naive23').text(naive[2][3]);
+        $('#naive30').text(naive[3][0]);
+        $('#naive31').text(naive[3][1]);
+        $('#naive32').text(naive[3][2]);
+        $('#naive33').text(naive[3][3]);
+        $('#opt00').text(opt[0][0]);
+        $('#opt01').text(opt[0][1]);
+        $('#opt02').text(opt[0][2]);
+        $('#opt03').text(opt[0][3]);
+        $('#opt10').text(opt[1][0]);
+        $('#opt11').text(opt[1][1]);
+        $('#opt12').text(opt[1][2]);
+        $('#opt13').text(opt[1][3]);
+        $('#opt20').text(opt[2][0]);
+        $('#opt21').text(opt[2][1]);
+        $('#opt22').text(opt[2][2]);
+        $('#opt23').text(opt[2][3]);
+        $('#opt30').text(opt[3][0]);
+        $('#opt31').text(opt[3][1]);
+        $('#opt32').text(opt[3][2]);
+        $('#opt33').text(opt[3][3]);
+      });
+    });
+  });
+});
